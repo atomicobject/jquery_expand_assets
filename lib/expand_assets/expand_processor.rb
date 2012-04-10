1fd149e7976caf89ff75938b210df26d615c79cf
Matt Fletcher <fletcher@atomicobject.com>
2012-04-09 20:57:27 -0400

    produce coffeescript instead of javascript

--- lib/expand_assets/expand_processor.rb
+++ lib/expand_assets/expand_processor.rb
@@ -2,8 +2,6 @@ require 'tilt'
 
 module ExpandAssets
   class ExpandProcessor < Tilt::Template
-    self.default_mime_type = 'application/javascript'
-
     class << self
       # A proc that is called to modify the template name used as the
       # JST key. The proc is passed the name as an argument and should
@@ -25,21 +23,18 @@ module ExpandAssets
     attr_reader :namespace
 
     def evaluate(scope, locals, &block)
-      p "evaluating #{scope.logical_path.inspect}"
-      p data
-      # "function(){$(#{data.inspect}).expand();}"
       name = scope.logical_path
       name = self.class.name_filter.call(name)
-      <<-JST
-(function() {
-  #{namespace} || (#{namespace} = {});
-  var template = null;
-  #{namespace}[#{name.inspect}] = function(directive) { 
-    template = template || $(#{data.inspect});
-    return template.expand(directive);
-  };
-}).call(this);
-      JST
+      <<-COFFEE
+        #{namespace} ||= {}
+        template = null
+        html = """
+               #{data}
+               """
+        #{namespace}[#{name.inspect}] = (directive) ->
+          template ||= $(html)
+          template.expand(directive)
+      COFFEE
     end
 
     private
